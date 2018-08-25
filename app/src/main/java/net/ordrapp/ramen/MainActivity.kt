243956f3e2f6c674a81434fd532843b072d7237d
VokusX <davidvoicu@rogers.com>
2018-08-25 01:11:56 -0400

    ordr-4 Center map to user location on app launch

--- app/src/main/java/net/ordrapp/ramen/MainActivity.kt
+++ app/src/main/java/net/ordrapp/ramen/MainActivity.kt
@@ -9,13 +9,17 @@ import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var fusedLocationClient: FusedLocationProviderClient
-    private lateinit var userLocation: Location
+    private var userLocation: Location? = null
+    private lateinit var mainMap: GoogleMap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,10 +36,26 @@ class MainActivity : AppCompatActivity() {
         }
 
         else {
-            fusedLocationClient.lastLocation.addOnSuccessListener { location ->  userLocation = location!!}
+            fusedLocationClient.lastLocation.addOnSuccessListener { location ->  userLocation = location!! }
+            centerToLocation()
         }
 
         val map = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
-        map.getMapAsync {}
+        map.getMapAsync {setMap(it)}
+
     }
+
+     private fun setMap(googleMap: GoogleMap) {
+         mainMap = googleMap
+         centerToLocation()
+    }
+
+    private fun centerToLocation(){
+        userLocation ?: return
+        if(::mainMap.isInitialized) {
+            val lastLocation = LatLng(userLocation!!.latitude, userLocation!!.longitude)
+            mainMap.moveCamera(CameraUpdateFactory.newLatLng(lastLocation))
+        }
+    }
+
 }
