3f23dc773d9083e862426ebde7d88deeca990ac0
derek <derek@llamabagel.ca>
2018-08-25 01:17:19 -0400

    Merged code Added Onboarding UI Added custom font for title?

--- app/src/main/java/net/ordrapp/ramen/MainActivity.kt
+++ app/src/main/java/net/ordrapp/ramen/MainActivity.kt
@@ -1,6 +1,7 @@
 package net.ordrapp.ramen
 
 import android.Manifest
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.location.Location
 import android.os.Bundle
@@ -14,47 +15,50 @@ import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import kotlinx.android.synthetic.main.activity_main.*
+import net.ordrapp.ramen.ui.OnboardingActivity
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private val fusedLocationClient: FusedLocationProviderClient by lazy { LocationServices.getFusedLocationProviderClient(this) }
     private var userLocation: Location? = null
-    private lateinit var mainMap: GoogleMap
+    private lateinit var googleMap: GoogleMap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         setSupportActionBar(toolbar)
+        supportActionBar?.title = ""
 
-        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
-        
+        // if (userHasNotSignedIn) {
+        startActivity(Intent(this, OnboardingActivity::class.java))
+        // }
+
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
                 != PackageManager.PERMISSION_GRANTED) {
-
             ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 0)
-        }
-
-        else {
-            fusedLocationClient.lastLocation.addOnSuccessListener { location ->  userLocation = location!! }
-            centerToLocation()
+        } else {
+            fusedLocationClient.lastLocation.addOnSuccessListener { location ->
+                userLocation = location!!
+                centerToLocation()
+            }
         }
 
         val map = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
-        map.getMapAsync {setMap(it)}
+        map.getMapAsync { googleMap = it }
+
+        map.getMapAsync {
+            googleMap = it
+            centerToLocation()
+        }
 
     }
 
-     private fun setMap(googleMap: GoogleMap) {
-         mainMap = googleMap
-         centerToLocation()
-    }
-
-    private fun centerToLocation(){
+    private fun centerToLocation() {
         userLocation ?: return
-        if(::mainMap.isInitialized) {
+        if (::googleMap.isInitialized) {
             val lastLocation = LatLng(userLocation!!.latitude, userLocation!!.longitude)
-            mainMap.moveCamera(CameraUpdateFactory.newLatLng(lastLocation))
+            googleMap.moveCamera(CameraUpdateFactory.newLatLng(lastLocation))
         }
     }
 
---
+++ app/src/main/java/net/ordrapp/ramen/ui/OnboardingActivity.kt
@@ -0,0 +1,13 @@
+package net.ordrapp.ramen.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import net.ordrapp.ramen.R
+
+class OnboardingActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_onboarding)
+    }
+}
