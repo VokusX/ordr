3f9f087d14bed50a9222db411fd2aa62208acf20
derek <derek@llamabagel.ca>
2018-08-25 11:37:50 -0400

    Added a User model class

---
+++ app/src/main/java/net/ordrapp/ramen/data/User.kt
@@ -0,0 +1,13 @@
+package net.ordrapp.ramen.data
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "users")
+data class User(@ColumnInfo(name = "name") val name: String,
+                @ColumnInfo(name = "givenName") val givenName: String?,
+                @ColumnInfo(name = "familyName") val familyName: String,
+                @ColumnInfo(name = "email") val email: String,
+                @ColumnInfo(name = "id") @PrimaryKey val id: String,
+                @ColumnInfo(name = "photo") val photo: String)
