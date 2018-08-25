c08ab1e04c3256724b0ad1e0621b92badd21a4d4
VokusX <davidvoicu@rogers.com>
2018-08-25 00:33:07 -0400

    ordr-1 Handle Location permissions requests (runtime permissions)

--- app/src/main/java/net/ordrapp/ramen/MainActivity.kt
+++ app/src/main/java/net/ordrapp/ramen/MainActivity.kt
@@ -1,26 +1,38 @@
 package net.ordrapp.ramen
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.gms.maps.GoogleMap
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.SupportMapFragment
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var googleMap: GoogleMap
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var loc: Location
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
 
-        setSupportActionBar(toolbar)
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 0)
+        }
 
-        val map = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        else {
+            fusedLocationClient.lastLocation.addOnSuccessListener { location: Location? ->  loc}
 
-        map.getMapAsync {
-            googleMap = it
+            val map = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+            map.getMapAsync {}
+
         }
     }
-
 }
