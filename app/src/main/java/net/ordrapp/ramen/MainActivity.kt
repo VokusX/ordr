1fcf91a371efe589132ff4e3ec7e273d45736f7a
derek <derek@llamabagel.ca>
2018-08-24 23:21:07 -0400

    Added Google Maps SDK Added Retrofit GSON and RxJava converters

--- app/src/main/java/net/ordrapp/ramen/MainActivity.kt
+++ app/src/main/java/net/ordrapp/ramen/MainActivity.kt
@@ -1,12 +1,20 @@
 package net.ordrapp.ramen
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.maps.SupportMapFragment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val map = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+
+        map.getMapAsync {
+
+        }
     }
+
 }
