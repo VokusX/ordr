86b6643427cfa951e280b6eb7cb28618a3f1d8ec
VokusX <davidvoicu@rogers.com>
2018-08-26 07:48:25 -0400

    MenuItem data

--- app/src/main/java/net/ordrapp/ramen/data/MenuItem.kt
+++ app/src/main/java/net/ordrapp/ramen/data/MenuItem.kt
@@ -9,8 +9,15 @@ data class MenuItem(@SerializedName("uuid") @PrimaryKey @ColumnInfo(name = "uuid
                     @SerializedName("price") @ColumnInfo(name = "price") val price: Double,
                     @SerializedName("calories") @ColumnInfo(name = "calories") val calories: Long,
                     @SerializedName("photo") @ColumnInfo(name = "photo") val photo: String,
-                    @SerializedName("vegetarian") @ColumnInfo(name = "vegetarian") val vegetarian: Boolean,
-                    @SerializedName("vegan") @ColumnInfo(name = "vegan") val vegan: Boolean,
-                    @SerializedName("spicy") @ColumnInfo(name = "spicy") val spicy: Boolean,
-                    @SerializedName("gluten_free") @ColumnInfo(name = "glutenFree") val glutenFree: Boolean) {
+                    @SerializedName("vegetarian") @ColumnInfo(name = "vegetarian") val vegetarian: Boolean = false,
+                    @SerializedName("vegan") @ColumnInfo(name = "vegan") val vegan: Boolean = false,
+                    @SerializedName("spicy") @ColumnInfo(name = "spicy") val spicy: Boolean = false,
+                    @SerializedName("gluten_free") @ColumnInfo(name = "glutenFree") val glutenFree: Boolean = false) {
 }
+
+val HAMBURGER = MenuItem("hamburger", "Hamburger", 3.99, 550, "https://images.unsplash.com/photo-1520072959219-c595dc870360?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=80a0e355a5f83d806f9b72782c7fa19b&auto=format&fit=crop&w=2053&q=80")
+val CHEESEBURGER = MenuItem("cheeseburger", "Cheeseburger", 4.99, 600, "https://images.unsplash.com/photo-1525164286253-al 04e68b9d94c6?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=957038b3b13d6e9a0dece74e4ffa33b8&auto=format&fit=crop&w=668&q=80")
+
+val SCRAMBLED_EGGS = MenuItem("eggs", "Scrambled Eggs", 2.99, 300, "https://images.unsplash.com/photo-1524250981170-bd522ef63fbf?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=9f18b175dbfd17d1511d924ad03aab2f&auto=format&fit=crop&w=668&q=80")
+val PANCAKES = MenuItem("pancakes", "Pancakes", 3.50, 400, "https://images.unsplash.com/photo-1528301392571-0dfab3c00216?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=5d9cf7b5db701b7e7a025f413d16d162&auto=format&fit=crop&w=1000&q=80")
+val FRENCH_TOAST = MenuItem("french_toast", "French Toast", 5.99, 700, "https://images.unsplash.com/photo-1528945522327-7032b0774637?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=f404c3049358c90de23a4c76595ac02a&auto=format&fit=crop&w=668&q=80")
